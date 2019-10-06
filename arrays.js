caf06579515d2166f60d30f7ac9a1c607969c67a
free-writer-5650 <miles.higbie@gmail.com>
2019-10-06 14:24:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,3 +13,8 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars,candyString){
 function addElementToEndOfArray(chocolateBars,candyString) {
 return [...chocolateBars,candyString];
 }
+
+function destructivelyAddElementToEndOfArray(chocolateBars,candyString){
+  chocolateBars.push(candyString)
+  return chocolateBars
+}
